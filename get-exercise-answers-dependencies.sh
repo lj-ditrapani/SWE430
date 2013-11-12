3dee5e70c73a21a7ee4a76b04f206c4f306cc810
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2013-11-12 13:11:33 -0500

    Create get-exercise-answers-dependencies.sh
    Copies dependencies of exercise answers into
    exam-and-exercise-answers/exercise and
    exam-and-exercise-answers/exercise-first-steps folders.  The
    dependencies are found in:
        - student-dev-tools
        - website/js
        - website/css

---
+++ get-exercise-answers-dependencies.sh
@@ -0,0 +1,9 @@
+dir1=exam-and-exercise-answers/exercise
+dir2=exam-and-exercise-answers/exercise-first-steps
+tools=student-dev-tools
+cp ${tools}/exercise-test.html $dir1
+cp ${tools}/exercise-test.html $dir2
+cp website/js/qunit.js $dir1
+cp website/js/qunit.js $dir2
+cp website/css/qunit.css $dir1
+cp website/css/qunit.css $dir2
