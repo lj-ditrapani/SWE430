3b3a4dcec13e562f8de4955fa56ed7f51ac23fdf
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2013-11-15 10:28:30 -0500

    Use etree to build html file and add prettyprint

--- convert-markdown.py
+++ convert-markdown.py
@@ -1,4 +1,6 @@
 import sys
+import json
+import xml.etree.ElementTree as ET
 import markdown2
 
 
@@ -10,16 +12,34 @@ def get_text(file_name):
 
 
 def convert(file_name):
-    text = get_text(file_name)
-    return markdown2.markdown(text)
+    markdown_text = get_text('markdown/{0}.markdown'.format(file_name))
+    html_text = markdown2.markdown(markdown_text)
+    body = ET.fromstring('<body>\n{0}\n</body>'.format(html_text))
+    return body
+
+
+def init_tree():
+    tree = ET.ElementTree()
+    tree.parse('markdown/boilerplate.html')
+    return tree
+
+
+def get_json(file_name):
+    return json.load(open('markdown/{0}.json'.format(file_name)))
+
 
 file_name = 'exercise'
-html = convert('markdown/{0}.markdown'.format(file_name))
-#head = get_text('html-fragments/head.html')
-head = get_text('markdown/head.html')
-
+tree = init_tree()
+config = get_json(file_name)
+html = tree.getroot()
+body = convert(file_name)
+html.append(body)
+pre_list = tree.findall('body/pre')
+for pre in pre_list:
+    pre.set('class', 'prettyprint')
+if config['prettyprint']:
+    body.set('onload', 'prettyPrint()')
+# print ET.tostring(tree.getroot())
 f = open('website/{0}.html'.format(file_name), 'w')
-f.write(head)
-f.write(html)
-f.write('\n</body></html>\n')
+tree.write(f)
 f.close()
