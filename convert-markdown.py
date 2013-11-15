73c17fdc1ea26e1e2d7841562ff0223c5998d185
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2013-11-15 13:24:57 -0500

    Add insert CSS and JavaScript to markdown workflow

--- convert-markdown.py
+++ convert-markdown.py
@@ -29,17 +29,32 @@ def get_json(file_name):
 
 
 file_name = 'exercise'
-tree = init_tree()
-config = get_json(file_name)
-html = tree.getroot()
 body = convert(file_name)
+tree = init_tree()
+html = tree.getroot()
 html.append(body)
 pre_list = tree.findall('body/pre')
 for pre in pre_list:
     pre.set('class', 'prettyprint')
+config = get_json(file_name)
 if config['prettyprint']:
     body.set('onload', 'prettyPrint()')
-# print ET.tostring(tree.getroot())
+head = html.find('head')
+title = ET.Element('title')
+title.text = config['title']
+title.tail = '\n'
+head.insert(1, title)
+for css_file_name in config['css']:
+    attrib = dict(rel='stylesheet', href=css_file_name, type='text/css')
+    e = ET.Element('link', attrib)
+    e.tail = '\n'
+    head.append(e)
+for js_file_name in config['js']:
+    attrib = dict(type='text/javascript', src=js_file_name)
+    e = ET.Element('script', attrib)
+    e.text = ' '
+    e.tail = '\n'
+    head.append(e)
 f = open('website/{0}.html'.format(file_name), 'w')
 tree.write(f)
 f.close()
