8a419916a504d0b22930e3006c839fe61a8d45bd
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2013-11-22 13:34:22 -0500

    Convert zip-website from sh to python

--- zip_website.py
+++ zip_website.py
@@ -1,4 +1,18 @@
-name="SWE430.zip"
-rm -f ${name}
-cd website
-zip -r ../${name} .
+#! /usr/bin/env python
+import os
+import zipfile
+
+
+def zip_folder(folder_name, zip_name):
+    rootlen = len(folder_name) + 1
+    zip_file = zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED)
+    for base, folders, files in os.walk(folder_name):
+        for file in files:
+            file_name = os.path.join(base, file)
+            zip_file.write(file_name, file_name[rootlen:])
+
+
+zip_name = 'SWE430.zip'
+if os.path.exists(zip_name):
+    os.remove(zip_name)
+zip_folder('website', zip_name)
