419a0e68a3a9784f107b663631927169e0f5303e
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2013-11-27 09:49:50 -0500

    Refactor assignment1-dev/test.js

--- assignment1-dev/test.js
+++ assignment1-dev/test.js
@@ -10,33 +10,55 @@
     'use strict';
 
 
+    function runTests(function_name, tests) {
+        var i, pair;
+        for (i = 0; i < tests.length; i += 1) {
+            triple = tests[i];
+            result = basicMath[function_name](triple[0], triple[1])
+            equal(result, triple[2]);
+        }
+    }
+
+
     test('Add', function() {
-        equal(basicMath.add(0, 0), 0);
-        equal(basicMath.add(5, 2), 7);
-        equal(basicMath.add(-12, 10), -2);
+        var tests = [
+            [0, 0, 0],
+            [5, 2, 7],
+            [-12, 10, -2]
+        ];
+        runTests('add', tests);
     });
 
 
     test('Subtract', function() {
-        equal(basicMath.subtract(0, 0), 0);
-        equal(basicMath.subtract(3, 7), -4);
-        equal(basicMath.subtract(-12, -15), 3);
+        var tests = [
+            [0, 0, 0],
+            [3, 7, -4],
+            [-12, -15, 3]
+        ];
+        runTests('subtract', tests);
     });
 
 
     test('Multiply', function() {
-        equal(basicMath.multiply(0, 0), 0);
-        equal(basicMath.multiply(3, 7), 21);
-        equal(basicMath.multiply(9, -4), -36);
-        equal(basicMath.multiply(-2, -5), 10);
-        equal(basicMath.multiply(-6, 1), -6);
+        var tests = [
+            [0, 0, 0],
+            [3, 7, 21],
+            [9, -4, -36],
+            [-2, -5, 10],
+            [-6, 1, -6]
+        ];
+        runTests('multiply', tests);
     });
 
 
     test('Divide', function() {
+        var tests = [
+            [16, 5, 3.2],
+            [-9, -3, 3]
+        ];
         ok(isNaN(basicMath.divide(0, 0)));
-        equal(basicMath.divide(16, 5), 3.2);
-        equal(basicMath.divide(-9, -3), 3);
+        runTests('divide', tests);
     });
 
 
