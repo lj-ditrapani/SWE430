32f77a030593a755983e077aec4a0480f1a0b6f2
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2013-11-27 08:53:36 -0500

    Assignment1 test.js first 2 tests

--- assignment1-dev/test.js
+++ assignment1-dev/test.js
@@ -1,1 +1,27 @@
-// assignment 1
+/**
+ * Author:  Lyall Jonathan Di Trapani ==================================
+ * Assignment 1
+ * basicMath library
+ */
+
+
+/*global test,equal,stringStat*/
+(function() {
+    'use strict';
+
+
+    test('Add', function() {
+        equal(basicMath.add(0, 0), 0)
+        equal(basicMath.add(5, 2), 7)
+        equal(basicMath.add(-12, 10), -2)
+    });
+
+
+    test('Subtract', function() {
+        equal(basicMath.subtract(0, 0), 0)
+        equal(basicMath.subtract(3, 7), -4)
+        equal(basicMath.subtract(-12, -15), 3)
+    });
+
+
+}());
