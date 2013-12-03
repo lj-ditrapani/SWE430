cb521edadf214029a388b3a11b6d966244e2a6a4
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2013-12-03 09:54:19 -0500

    Example 2 milestone 02

---
+++ website/example2/test02.js
@@ -0,0 +1,17 @@
+/**
+ * Author:  Lyall Jonathan Di Trapani ==================================
+ * Example 2 Test Code
+ * Test the roman module
+ */
+
+/*global test,equal,roman*/
+(function() {
+    'use strict';
+
+
+    test('I -> 1', function() {
+        equal(roman.toDecimal('I'), 1);
+    });
+
+
+}());
