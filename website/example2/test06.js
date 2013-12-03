e6e17bfb5f5bc0b4d2d6b67f1b02362ae7337e47
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2013-12-03 11:07:00 -0500

    Example 2 milestone 06

---
+++ website/example2/test06.js
@@ -0,0 +1,36 @@
+/**
+ * Author:  Lyall Jonathan Di Trapani ==================================
+ * Example 2 Test Code
+ * Test the roman module
+ */
+
+/*global test,equal,roman*/
+(function() {
+    'use strict';
+
+
+    function runTests(tests) {
+        var i, pair, label;
+        for (i = 0; i < tests.length; i += 1) {
+            pair = tests[i];
+            label = pair[0] + ' -> ' + pair[1];
+            equal(roman.toDecimal(pair[0]), pair[1], label);
+        }
+    }
+
+
+    test('Individual Roman Numerals', function() {
+        var tests = [
+            ['I', 1],
+            ['V', 5],
+            ['X', 10],
+            ['L', 50],
+            ['C', 100],
+            ['D', 500],
+            ['M', 1000]
+        ];
+        runTests(tests);
+    });
+
+
+}());
