b85298398f21e4e8dd961820dc6e58948db39063
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2013-12-03 09:53:27 -0500

    Example 2 1 test passing

--- markdown/example2.markdown
+++ markdown/example2.markdown
@@ -5,3 +5,20 @@ Example 2
 - [Example 2 jsbin Environment][jsbin example 2]
 
 [jsbin example 2]: http://jsbin.com/swe430_example2/latest/edit?javascript,live
+
+
+Initial skeleton, module function wrapper
+
+'roman' is undefined
+
+export roman name-space
+
+Object doesn't support property or method 'toDecimal'
+
+create empty toDecimal method
+
+Failed
+- expected: 1
+- result: undefined
+
+Return 1
---
+++ website/example2/code.js
@@ -0,0 +1,23 @@
+/**
+ * Author:  Lyall Jonathan Di Trapani ==================================
+ * Example 2 Production Code
+ * The roman module contains functions to convert roman numerals into
+ * decimal numbers.
+ */
+
+/*jslint browser: true */
+(function() {
+    'use strict';
+
+
+    function toDecimal() {
+        return 1;
+    }
+
+
+    window.roman = {
+        toDecimal: toDecimal
+    };
+
+
+}());
